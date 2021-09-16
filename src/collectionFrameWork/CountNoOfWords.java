1f173702205624518f42f0dd5c6430ce892e57cb
Devesh Vishal <deveshvishal.607@gmail.com>
2021-09-16 10:24:41 +0530

    second commit

--- src/collectionFrameWork/CountNoOfWords.java
+++ src/collectionFrameWork/CountNoOfWords.java
@@ -1,9 +1,30 @@
 package collectionFrameWork;
 
+import java.util.HashMap;
+
 public class CountNoOfWords {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		HashMap<String,Integer> mp = new HashMap<>();
+		
+		String s = "Java is a program language , it is a platform independent language";
+		
+		String words[] = s.split(" ");
+		
+		for(String word : words) {
+			
+			Integer integer = mp.get(word);
+			  
+            if (integer == null)
+                mp.put(word, 1);
+  
+            else {
+                mp.put(word, integer + 1);
+            }
+        }
+		
+        System.out.println(mp);
 
 	}
 
